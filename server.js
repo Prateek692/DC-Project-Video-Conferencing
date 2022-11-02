33fa677c666b3da5e5f81871c0b37d90e9fe58a9
Prateek692 <prateek692@hotmail.com>
2022-11-02 21:10:12 +0530

    Initialize first view DONE

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express=require('express');
 const app=express();
 // Server which we would run
 const server=require('http').Server(app);
-
+app.set('view engine','ejs');                   //App's view engine set to ejs
 app.get('/', (req,res)=>{
-    res.status(200).send("Hello World");
+    res.render('room');
 }
 )  //Root URL where app is kept
-
-
 
 
 server.listen(3030); //Localhost port would be 3030
