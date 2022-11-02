3fdb06064a064ae13b87149cc08d11ef4b2c6f5f
Prateek692 <prateek692@hotmail.com>
2022-11-02 21:31:43 +0530

    Create a room id DONE

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express=require('express');
 const app=express();
-// Server which we would run
-const server=require('http').Server(app);
+const server=require('http').Server(app);       // Server which we would run
+const {v4:uuidv4}=require('uuid');              //Importing uuid version v4
 app.set('view engine','ejs');                   //App's view engine set to ejs
-app.get('/', (req,res)=>{
-    res.render('room');
+
+
+
+
+
+app.get('/', (req,res)=>{                       //Root URL where app is kept
+    res.redirect(`/${uuidv4()}`);               //This is main route. When server runs, uuid is auto generated and redirect the app to it. 
 }
-)  //Root URL where app is kept
+)  
+
+app.get('/:room',(req,res)=>{                   // ${uuidv4()} is accepted here as uuidv4()=room and is redirected here.
+    res.render('room',{roomId:req.params.room}); //2nd parameter passes RoomId to room.ejs
+})
+
 
 
 server.listen(3030); //Localhost port would be 3030
